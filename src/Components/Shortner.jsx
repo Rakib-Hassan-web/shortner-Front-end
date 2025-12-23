3f599e17e879b43939777169b9fbe2898b0c3e54
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-23 23:54:55 +0600

    short url making done now registration

--- src/Components/Shortner.jsx
+++ src/Components/Shortner.jsx
@@ -10,13 +10,7 @@ const Shortner = () => {
     if (!longUrl) return;
 
 
-    try {
-      const res = await Urservices.createshort(longUrl);
-      
-      setShortUrl(`http://localhost:8000/${res.ShortUrl}`);
-      console.log(res)
-    } catch (error) {
-      console.log(error);
+    
      
     }
     
